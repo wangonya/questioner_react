e64b97c7402aab51c7cfec793f15deacaabe360e
wangonya <kwangonya@gmail.com>
2019-05-02 08:49:58 +0300

    setup navbar and footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Menu, Container} from "semantic-ui-react";
+
+const Footer = () => (
+    <div>
+        <Menu fixed='bottom' inverted borderless>
+            <Container>
+                <Menu.Item>
+                    Footer
+                </Menu.Item>
+            </Container>
+        </Menu>
+    </div>
+);
+
+export default Footer;
